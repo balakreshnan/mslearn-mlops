6d3f2d5d7234acccf8dde9ef156c5744a9671370
Balakreshnan <bala_b@hotmail.com>
2022-08-26 09:38:24 -0500

    linting update

--- src/model/score.py
+++ src/model/score.py
@@ -7,18 +7,18 @@ import joblib
 
 def init():
     """
-    This function is called when the container is 
-    initialized/started, typically after 
+    This function is called when the container is
+    initialized/started, typically after
     create/update of the deployment.
-    You can write the logic here to perform init 
+    You can write the logic here to perform init
     operations like caching the model in memory
     """
     global model
-    # AZUREML_MODEL_DIR is an environment variable 
+    # AZUREML_MODEL_DIR is an environment variable
     # created during deployment.
-    # It is the path to the model folder 
+    # It is the path to the model folder
     # (./azureml-models/$MODEL_NAME/$VERSION)
-    # Please provide your model's folder 
+    # Please provide your model's folder
     # name if there is one
     model_path = os.path.join(
         os.getenv("AZUREML_MODEL_DIR"), "model/model.pkl"
@@ -30,9 +30,9 @@ def init():
 
 def run(raw_data):
     """
-    This function is called for every invocation of the 
+    This function is called for every invocation of the
     endpoint to perform the actual scoring/prediction.
-    In the example we extract the data from the json 
+    In the example we extract the data from the json
     input and call the scikit-learn model's predict()
     method and return the result back
     """
